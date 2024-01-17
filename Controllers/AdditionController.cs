fbb8a5541fbdff4b6133ebd5df95dd54cd4ceabf
EricaKl <eriKrishna21@gmail.com>
2024-01-17 23:28:41 +0530

    commit

---
+++ Controllers/AdditionController.cs
@@ -0,0 +1,22 @@
+﻿using CorePrcatice1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePrcatice1.Controllers
+{
+    public class AdditionController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Number number = new Number();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Number number)
+        {
+            ViewBag.Result = number.number1 + number.number2;
+            
+            return View();
+        }
+    }
+}
---
+++ Models/Number.cs
@@ -0,0 +1,8 @@
+﻿namespace CorePrcatice1.Models
+{
+    public class Number
+    {
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+    }
+}
